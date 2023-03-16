9b6d5701e0cdb1a6e27e0ca33194239d96dee9a3
Rahul Agnihotri <rahulagnihotri1998@gmail.com>
2023-03-16 23:24:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
+import {useState} from 'react'
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [numbers,setNumbers]=useState([])
+  const [sum,setSum]=useState(0);
+  let addRemoveHandler=(e)=>{
+      let number=parseInt(e.target.value);
+      if(isNaN(number))
+      {
+        return;
+      }
+      let sign=number>=0?1:-1;
+      let absoluteNumber=Math.abs(number);
+      let newSum=sum+absoluteNumber*sign;
+      setNumbers([...numbers,number])
+      setSum(newSum);
+
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" onChange={addRemoveHandler}/>
+        <p>Sum:{sum}</p>
     </div>
   )
 }
