4f5b1131ff2af305101e981a52ea4cfdd5a201ce
Rahul Agnihotri <rahulagnihotri1998@gmail.com>
2023-03-16 23:28:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,30 @@
+import React, { useState, useEffect } from "react";
 
-import {useState} from 'react'
-import React from "react";
-import './../styles/App.css';
 
 const App = () => {
-  const [numbers,setNumbers]=useState([])
-  const [sum,setSum]=useState(0);
-  let addRemoveHandler=(e)=>{
-      let number=parseInt(e.target.value);
-      if(isNaN(number))
-      {
-        return;
-      }
-      let sign=number>=0?1:-1;
-      let absoluteNumber=Math.abs(number);
-      let newSum=sum+absoluteNumber*sign;
-      setNumbers([...numbers,number])
-      setSum(newSum);
+ const [numbers, setNumbers] = useState([]);
+ const [sum, setSum] = useState(0);
 
-  }
-  return (
-    <div>
-        <h1>Sum Calculator</h1>
-        <input type="number" onChange={addRemoveHandler}/>
-        <p>Sum:{sum}</p>
-    </div>
-  )
-}
 
-export default App
+ useEffect(() => {
+   setSum(numbers.reduce((acc, number) => acc + number, 0));
+ }, [numbers]);
+
+
+ const handleNumberInput = (event) => {
+   const number = parseInt(event.target.value);
+   setNumbers((prevNumbers) => [...prevNumbers, number]);
+ };
+
+
+ return (
+   <div>
+     <h1>Sum Calculator</h1>
+     <input type="number" onChange={handleNumberInput} />
+     <p>Sum: {sum}</p>
+   </div>
+ );
+};
+
+
+export default App;
